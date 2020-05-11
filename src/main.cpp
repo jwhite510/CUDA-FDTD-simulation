f9a80c4790e30fc457e43f3720a56a1b1c4e182f
jonathon <jonathonwhite@protonmail.com>
2020-05-11 04:07:40 +0200

    almost identical

--- src/main.cpp
+++ src/main.cpp
@@ -62,86 +62,6 @@ struct Tensor{
     }
 
 };
-
-void E_from_H(Tensor &E, Tensor &H, Tensor &J, Params &P){
-
-  for(int i=1; i < E.N_x-2; i++){
-    for(int j=1; j < E.N_y-2; j++){
-      for(int k=1; k < E.N_z-2; k++){
-
-        double Ec = (1/P.eps0)*(P.dt/P.dx);
-        double Jc = (1/P.eps0)*P.dt;
-
-        E.x(i,j,k)=E.x(i,j,k)+Ec*(H.z(i,j,k)-H.z(i,j-1,k))-Ec*(H.y(i,j,k)-H.y(i,j,k-1));
-        E.y(i,j,k)=E.y(i,j,k)+Ec*(H.x(i,j,k)-H.x(i,j,k-1))-Ec*(H.z(i,j,k)-H.z(i-1,j,k));
-        E.z(i,j,k)=E.z(i,j,k)+Ec*(H.y(i,j,k)-H.y(i-1,j,k))-Ec*(H.x(i,j,k)-H.x(i,j-1,k));
-
-        E.x(i,j,k) = E.x(i,j,k) - Jc*J.x(i,j,k);
-        E.y(i,j,k) = E.y(i,j,k) - Jc*J.y(i,j,k);
-        E.z(i,j,k) = E.z(i,j,k) - Jc*J.z(i,j,k);
-
-        // E.x(i,j,k) = E.x(i,j,k) +
-        //   Ec*(
-        //     (H.z(i,j,k) - H.z(i,j-1,k))
-        //   - (H.y(i,j,k) - H.y(i,j,k-1))
-        //   );
-        // E.x(i,j,k) -= Jc*J.x(i,j,k);
-
-        // E.y(i-1,j+1,k) = E.y(i-1,j+1,k) +
-        //   Ec*(
-        //     (H.x(i,j,k) - H.x(i,j,k-1))
-        //   - (H.z(i,j,k) - H.z(i-1,j,k))
-        //   );
-        // E.y(i,j,k) -= Jc*J.y(i,j,k);
-
-        // E.z(i,j,k) = E.z(i,j,k) +
-        //   Ec*(
-        //     (H.y(i,j,k) - H.y(i-1,j,k))
-        //   - (H.x(i,j,k) - H.x(i,j-1,k+1))
-        //   );
-        // E.z(i,j,k) -= Jc*J.z(i,j,k);
-
-        // double jvalx = (P.dt/P.eps0)*J.x(i,j,k);
-        // double jvaly = (P.dt/P.eps0)*J.y(i,j,k);
-        // double jvalz = (P.dt/P.eps0)*J.z(i,j,k);
-
-      }
-    }
-  }
-}
-void H_from_E(Tensor &H, Tensor &E, Params &P){
-
-  double Hc = (1/P.mu0)*(P.dt/P.dx);
-
-  for(int i=1; i < E.N_x-2; i++){
-    for(int j=1; j < E.N_y-2; j++){
-      for(int k=1; k < E.N_z-2; k++){
-
-        H.x(i,j,k)=H.x(i,j,k)+Hc*(E.y(i,j,k+1)-E.y(i,j,k))-Hc*(E.z(i,j+1,k)-E.z(i,j,k));
-        H.y(i,j,k)=H.y(i,j,k)+Hc*(E.z(i+1,j,k)-E.z(i,j,k))-Hc*(E.x(i,j,k+1)-E.x(i,j,k));
-        H.z(i,j,k)=H.z(i,j,k)+Hc*(E.x(i,j+1,k)-E.x(i,j,k))-Hc*(E.y(i+1,j,k)-E.y(i,j,k));
-
-        // H.x(i,j,k) = H.x(i,j,k) +
-        //   Hc*(
-        //     ((E.y(i,j,k+1) - E.y(i,j,k)))
-        //   - ((E.z(i,j+1,k) - E.z(i,j,k)))
-        //   );
-
-        // H.y(i,j,k+1) = H.y(i,j,k+1) +
-        //   Hc*(
-        //     ((E.z(i+1,j,k) - E.z(i,j,k)))
-        //   - ((E.x(i,j,k+1) - E.x(i,j,k)))
-        //   );
-
-        // H.z(i,j+1,k) = H.z(i,j+1,k) +
-        //   Hc*(
-        //     ((E.x(i,j+1,k) - E.x(i,j,k)))
-        //   - ((E.y(i+1,j,k) - E.y(i,j,k)))
-        //   );
-      }
-    }
-  }
-}
 int main()
 {
   std::cout << "hello" << std::endl;
@@ -171,22 +91,44 @@ int main()
   MakeGaussian(gaussian);
 
   double t=0;
+  double Hc = (1/P.mu0)*(P.dt/P.dx);
+  double Ec = (1/P.eps0)*(P.dt/P.dx);
+  double Jc = (1/P.eps0)*P.dt;
   for(int i=0;i<300;i++){
     t+=P.dt;
 
-    for(int i=0; i < J.x.size_0; i++){
-      for(int j=0; j < J.x.size_1; j++){
-        for(int k=0; k < J.x.size_2; k++){
-          // J.z(i,j,k) = cos(omega*t)*gaussian(i,j);
-          J.z(i,j,k)=cos(omega*t)*exp(-(pow(i-50,2)/2))*exp(-(pow(j-50,2)/2))*exp(-(pow(k-50,2)/2));
+    for(int i=0; i < N_x; i++)
+      for(int j=0; j < N_y; j++)
+        for(int k=0; k < N_z; k++)
+          J.z(i,j,k)=exp(-(pow(i-50,2)/2))*exp(-(pow(j-50,2)/2))*exp(-(pow(k-50,2)/2));
+
+    cout << "t => " << t << endl;
+    // Python.call_function_np("plot", J.z.data, vector<int>{J.z.size_0,J.z.size_1,H.z.size_2}, PyArray_FLOAT64);
+    for(int i=1; i < E.N_x-2; i++){
+      for(int j=1; j < E.N_y-2; j++){
+        for(int k=1; k < E.N_z-2; k++){
+
+          E.x(i,j,k)=E.x(i,j,k)+Ec*(H.z(i,j,k)-H.z(i,j-1,k))-Ec*(H.y(i,j,k)-H.y(i,j,k-1));
+          E.y(i,j,k)=E.y(i,j,k)+Ec*(H.x(i,j,k)-H.x(i,j,k-1))-Ec*(H.z(i,j,k)-H.z(i-1,j,k));
+          E.z(i,j,k)=E.z(i,j,k)+Ec*(H.y(i,j,k)-H.y(i-1,j,k))-Ec*(H.x(i,j,k)-H.x(i,j-1,k));
+
+          E.x(i,j,k) = E.x(i,j,k) - Jc*J.x(i,j,k)*cos(omega*t);
+          E.y(i,j,k) = E.y(i,j,k) - Jc*J.y(i,j,k)*cos(omega*t);
+          E.z(i,j,k) = E.z(i,j,k) - Jc*J.z(i,j,k)*cos(omega*t);
+
         }
       }
     }
-    cout << "t => " << t << endl;
-    // Python.call_function_np("plot", J.z.data, vector<int>{J.z.size_0,J.z.size_1,H.z.size_2}, PyArray_FLOAT64);
+    for(int i=1; i < E.N_x-2; i++){
+      for(int j=1; j < E.N_y-2; j++){
+        for(int k=1; k < E.N_z-2; k++){
 
-    E_from_H(E, H, J, P);
-    H_from_E(H, E, P);
+          H.x(i,j,k)=H.x(i,j,k)+Hc*(E.y(i,j,k+1)-E.y(i,j,k))-Hc*(E.z(i,j+1,k)-E.z(i,j,k));
+          H.y(i,j,k)=H.y(i,j,k)+Hc*(E.z(i+1,j,k)-E.z(i,j,k))-Hc*(E.x(i,j,k+1)-E.x(i,j,k));
+          H.z(i,j,k)=H.z(i,j,k)+Hc*(E.x(i,j+1,k)-E.x(i,j,k))-Hc*(E.y(i+1,j,k)-E.y(i,j,k));
+        }
+      }
+    }
 
 
     Python.call_function_np("plot", H.x.data, vector<int>{H.x.size_0,H.x.size_1,H.x.size_2}, PyArray_FLOAT64);
