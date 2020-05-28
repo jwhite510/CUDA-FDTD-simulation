46f8085848b84ca3fb304e1b9f7fe6a60d3e110d
jonathon <jonathonwhite@protonmail.com>
2020-05-28 15:40:38 +0200

    converted to class

--- src/main.cpp
+++ src/main.cpp
@@ -8,41 +8,6 @@ using namespace std;
 
 // void E_from_H()
 
-void Linspace(vector<double> &vec, double min, double max)
-{
-  int N = vec.size();
-  double length = max - min;
-  double dx = length / N;
-  double val = min;
-  for(int i=0; i < vec.size(); i++){
-    vec[i]=val;
-    val+=dx;
-  }
-}
-
-void MakeGaussian(array2d<double> &gaussian){
-  // linspace x and y
-  vector<double>x(gaussian.size_0, 0);
-  Linspace(x,-1,1);
-
-  vector<double>y(gaussian.size_1, 0);
-  Linspace(y,-1,1);
-
-  double w=0.01;
-  for(int i=0; i < gaussian.size_0; i++){
-    for(int j=0; j < gaussian.size_1; j++){
-      gaussian(i,j) = exp(-pow(x[i],2)/w)*exp(-pow(y[j],2)/w);
-    }
-  }
-}
-struct Params{
-  double dt;
-  double eps0;
-  double mu0;
-  double dy;
-  double dx;
-  double dz;
-};
 struct Tensor{
   array3d<double> x;
   array3d<double> y;
@@ -64,41 +29,52 @@ struct Tensor{
     }
 
 };
-int main()
+struct FDTD
 {
-  std::cout << "hello" << std::endl;
+  Tensor E;
+  Tensor H;
+  Tensor J;
   // make arrays
-  int N_x = 50;
-  int N_y = 50;
-  int N_z = 50;
-
-  double c=2.998e8;
-  double mu0 = 4*M_PI*1e-7;
-  double eps0 = 8.85e-12;
-  double dx = 50e-9;
-  double dt = (50e-9)/(2*c);
-  double freq=500e12;
-  double omega = 2*M_PI*freq;
-
-  Tensor E(N_x, N_y, N_z);
-  Tensor H(N_x, N_y, N_z);
-  Tensor J(N_x, N_y, N_z);
-
-
-  double t=0;
-  double Hc = (1/mu0)*(dt/dx);
-  double Ec = (1/eps0)*(dt/dx);
-  double time_span=15e-15;
-  double Jc = (1/eps0)*dt;
-  int tmax_steps=time_span/dt;
-
-
-  for(int i=0; i < N_x; i++)
-    for(int j=0; j < N_y; j++)
-      for(int k=0; k < N_z; k++)
-        J.z(i,j,k)=exp(-(pow(i-25,2)/2))*exp(-(pow(j-25,2)/2))*exp(-(pow(k-25,2)/2));
-
-  for(int n=0; n < tmax_steps; n++){
+  int N_x ;
+  int N_y ;
+  int N_z ;
+  double c;
+  double mu0 ;
+  double eps0 ;
+  double dx ;
+  double dt ;
+  double freq;
+  double omega ;
+  double t;
+  double Hc ;
+  double Ec ;
+  double time_span;
+  double Jc ;
+  int tmax_steps;
+  FDTD():
+    // make arrays
+    N_x ( 50),
+    N_y ( 50),
+    N_z ( 50),
+    c(2.998e8),
+    mu0 ( 4*M_PI*1e-7),
+    eps0 ( 8.85e-12),
+    dx ( 50e-9),
+    dt ( (50e-9)/(2*c)),
+    freq(500e12),
+    t(0),
+    Hc ( (1/mu0)*(dt/dx)),
+    Ec ( (1/eps0)*(dt/dx)),
+    time_span(15e-15),
+    Jc ( (1/eps0)*dt),
+    omega ( 2*M_PI*freq),
+    tmax_steps(time_span/dt),
+    E(50, 50, 50),
+    H(50, 50, 50),
+    J(50, 50, 50)
+  {
+  }
+  void timestep(int n){
     for(int i=1; i < E.N_x-2; i++){
       for(int j=1; j < E.N_y-2; j++){
         for(int k=1; k < E.N_z-2; k++){
@@ -110,6 +86,9 @@ int main()
           E.x(i,j,k) = E.x(i,j,k) - Jc*J.x(i,j,k)*cos(omega*n*dt);
           E.y(i,j,k) = E.y(i,j,k) - Jc*J.y(i,j,k)*cos(omega*n*dt);
           E.z(i,j,k) = E.z(i,j,k) - Jc*J.z(i,j,k)*cos(omega*n*dt);
+          // E.x(i,j,k) = E.x(i,j,k) - Jc*J.x(i,j,k);
+          // E.y(i,j,k) = E.y(i,j,k) - Jc*J.y(i,j,k);
+          // E.z(i,j,k) = E.z(i,j,k) - Jc*J.z(i,j,k);
 
         }
       }
@@ -124,14 +103,27 @@ int main()
         }
       }
     }
+  }
+};
+int main()
+{
+
+  FDTD fdtd;
+  for(int n=0; n < fdtd.tmax_steps; n++){
+    for(int i=0; i < fdtd.N_x; i++)
+      for(int j=0; j < fdtd.N_y; j++)
+        for(int k=0; k < fdtd.N_z; k++)
+          fdtd.J.z(i,j,k)=exp(-(pow(i-25,2)/2))*exp(-(pow(j-25,2)/2))*exp(-(pow(k-25,2)/2));
 
     // plot H.x
     // H.x
-    array2d<double>hx_display(H.x.size_0,H.x.size_1);
+    fdtd.timestep(n);
+
+    array2d<double>hx_display(fdtd.H.x.size_0,fdtd.H.x.size_1);
     int k=25;
-    for(int i=0; i < H.x.size_0; i++)
-      for(int j=0; j < H.x.size_1; j++)
-        hx_display(i,j)=H.x(i,j,k);
+    for(int i=0; i < fdtd.H.x.size_0; i++)
+      for(int j=0; j < fdtd.H.x.size_1; j++)
+        hx_display(i,j)=fdtd.H.x(i,j,k);
 
     if(n==0)
       hx_display.show("hx_display");
