6501e4a6488e31deb4874d70791b2c6d926a1407
jonathon <jonathonwhite@protonmail.com>
2020-05-28 15:58:01 +0200

    removing unecessary things from class

--- src/main.cpp
+++ src/main.cpp
@@ -41,40 +41,26 @@ struct FDTD
   double c;
   double mu0 ;
   double eps0 ;
-  double dx ;
-  double dt ;
-  double freq;
-  double omega ;
-  double t;
   double Hc ;
   double Ec ;
-  double time_span;
   double Jc ;
-  int tmax_steps;
-  FDTD():
+  FDTD(int N_x,int N_y,int N_z,double dx,double dt):
     // make arrays
-    N_x ( 50),
-    N_y ( 50),
-    N_z ( 50),
+    N_x ( N_x),
+    N_y ( N_y),
+    N_z ( N_z),
     c(2.998e8),
     mu0 ( 4*M_PI*1e-7),
     eps0 ( 8.85e-12),
-    dx ( 50e-9),
-    dt ( (50e-9)/(2*c)),
-    freq(500e12),
-    t(0),
     Hc ( (1/mu0)*(dt/dx)),
     Ec ( (1/eps0)*(dt/dx)),
-    time_span(15e-15),
     Jc ( (1/eps0)*dt),
-    omega ( 2*M_PI*freq),
-    tmax_steps(time_span/dt),
     E(50, 50, 50),
     H(50, 50, 50),
     J(50, 50, 50)
   {
   }
-  void timestep(int n){
+  void timestep(){
     for(int i=1; i < E.N_x-2; i++){
       for(int j=1; j < E.N_y-2; j++){
         for(int k=1; k < E.N_z-2; k++){
@@ -83,12 +69,9 @@ struct FDTD
           E.y(i,j,k)=E.y(i,j,k)+Ec*(H.x(i,j,k)-H.x(i,j,k-1))-Ec*(H.z(i,j,k)-H.z(i-1,j,k));
           E.z(i,j,k)=E.z(i,j,k)+Ec*(H.y(i,j,k)-H.y(i-1,j,k))-Ec*(H.x(i,j,k)-H.x(i,j-1,k));
 
-          E.x(i,j,k) = E.x(i,j,k) - Jc*J.x(i,j,k)*cos(omega*n*dt);
-          E.y(i,j,k) = E.y(i,j,k) - Jc*J.y(i,j,k)*cos(omega*n*dt);
-          E.z(i,j,k) = E.z(i,j,k) - Jc*J.z(i,j,k)*cos(omega*n*dt);
-          // E.x(i,j,k) = E.x(i,j,k) - Jc*J.x(i,j,k);
-          // E.y(i,j,k) = E.y(i,j,k) - Jc*J.y(i,j,k);
-          // E.z(i,j,k) = E.z(i,j,k) - Jc*J.z(i,j,k);
+          E.x(i,j,k) = E.x(i,j,k) - Jc*J.x(i,j,k);
+          E.y(i,j,k) = E.y(i,j,k) - Jc*J.y(i,j,k);
+          E.z(i,j,k) = E.z(i,j,k) - Jc*J.z(i,j,k);
 
         }
       }
@@ -107,17 +90,30 @@ struct FDTD
 };
 int main()
 {
+  int N_x=50;
+  int N_y=50;
+  int N_z=50;
+  double dx=50e-9;
+  double c=2.998e8;
+  double dt=((dx)/(2*c));
 
-  FDTD fdtd;
-  for(int n=0; n < fdtd.tmax_steps; n++){
+  // for source term
+  double freq=500e12;
+  double omega =2*M_PI*freq;
+
+  double time_span=15e-15;
+  int tmax_steps=time_span/dt;
+
+  FDTD fdtd(N_x,N_y,N_z,dx,dt);
+  for(int n=0; n < tmax_steps; n++){
+
+    // define source term
     for(int i=0; i < fdtd.N_x; i++)
       for(int j=0; j < fdtd.N_y; j++)
         for(int k=0; k < fdtd.N_z; k++)
-          fdtd.J.z(i,j,k)=exp(-(pow(i-25,2)/2))*exp(-(pow(j-25,2)/2))*exp(-(pow(k-25,2)/2));
+          fdtd.J.z(i,j,k)=exp(-(pow(i-25,2)/2))*exp(-(pow(j-25,2)/2))*exp(-(pow(k-25,2)/2))*cos(omega*n*dt);
 
-    // plot H.x
-    // H.x
-    fdtd.timestep(n);
+    fdtd.timestep();
 
     array2d<double>hx_display(fdtd.H.x.size_0,fdtd.H.x.size_1);
     int k=25;
